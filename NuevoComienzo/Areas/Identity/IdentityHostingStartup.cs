851f610a6a5e596b2ffca062c67b7f19456092ed
cmartinezh184 <cmartinezh184@ulacit.ed.cr>
2019-07-07 18:51:35 -0600

    Se agregaron las vistas para modificar el login y se hizo en una nueva rama de pruebas para autenticacion

---
+++ NuevoComienzo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NuevoComienzo.Data;
+
+[assembly: HostingStartup(typeof(NuevoComienzo.Areas.Identity.IdentityHostingStartup))]
+namespace NuevoComienzo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
