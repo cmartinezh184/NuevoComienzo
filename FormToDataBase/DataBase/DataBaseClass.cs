e11d78023394ed0ff454ef25075a32e23aad240c
robertram <rsft6000@gmail.com>
2019-08-07 23:20:36 -0500

    8/7/2019 11:19pm Se añadió los cambios de agregar datos a diferentes tablas acontroller.cs

---
+++ FormToDataBase/DataBase/DataBaseClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace FormToDataBase.DataBase
+{
+    class Class1
+    {
+
+
+        private bool (){
+            HtmlElement htmlelement = web.Document.GetElementById("userInfoNav");
+            if (htmlelement == null)
+            {
+                return;
+            }
+            else
+            {
+                statsTextBox.Text = web.Document.GetElementById("userInfoNav").OuterText;
+            }
+
+}
+
+        private void Agregar()
+        {
+            string connetionString = null;
+            string sql = null;
+            connetionString = "Data Source=DESKTOP-ULLHLPN;Initial Catalog=DBForm;Integrated Security=True";
+            using (SqlConnection cnn = new SqlConnection(connetionString))
+            {
+                sql = "insert into Main ([Firt Name], [Last Name]) values(@first,@last)";
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                {
+                    cmd.Parameters.AddWithValue("@first", textbox2.text);
+                    cmd.Parameters.AddWithValue("@last", textbox3.text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Row inserted !! ");
+                }
+            }
+        }
+
+    }
+}
---
+++ FormToDataBase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FormToDataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
