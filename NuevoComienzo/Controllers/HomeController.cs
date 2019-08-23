9e652d32b45216ffcff49836c46b08adc4a6a55d
Katherine Martínez <36804849+kathymch@users.noreply.github.com>
2019-08-23 11:57:15 -0600

    23/8/2019 11:56am Cambios a HomeController y NuecoComienzp.csproj

--- NuevoComienzo/Controllers/HomeController.cs
+++ NuevoComienzo/Controllers/HomeController.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 using NuevoComienzo.Models;
 
 namespace NuevoComienzo.Controllers
 {
     public class HomeController : Controller
     {
+
+        public string PostedMessage { get; set; } = "";
+
+        [BindProperty]
+        public string name { get; set; }
+        [BindProperty]
+        public string email { get; set; }
+        [BindProperty]
+        public string message { get; set; }
         public IActionResult Index()
         {
-            return View();
+            /*
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Fundacion", "gabohernand123@gmail.com"));
+            message.To.Add(new MailboxAddress("Robert Ramirez", "rsft6000@gmail.com"));
+            message.Subject = "Fundación Un Nuevo Comienzo";
+            message.Body = new TextPart("plain")
+            {
+                Text = message + "Gracias por escribirnos {FromName}. La respuesta llegará en breve a su correo"
+            };
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, true);
+                client.Authenticate("gabohernand123@gmail.com", "Rosturama20");
+                client.Send(message);
+                client.Disconnect(true);
+            }*/
+
+            return View(email);
         }
 
         public IActionResult AboutUs()
